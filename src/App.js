d7ec2dd96c13b72a326e37733997eb9e1c00ae2e
Vimal <vimalkumarak478@gmail.com>
2024-06-08 00:02:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/shikha-vimal">
       <Navbar/>
         <Routes>
           <Route path='/' element={<Home/>}></Route>
